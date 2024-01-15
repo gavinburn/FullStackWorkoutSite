61d20d395073a9d45f5ca0d297c392e9a45da158
gavinburn <65696841+gavinburn@users.noreply.github.com>
2024-01-15 18:55:48 -0500

    Add files via upload

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,47 @@
+import { createContext, useReducer } from "react";
+import { useContext } from "react"
+
+export const WorkoutContext = createContext()
+
+
+export const workoutReducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_WORKOUTS':
+        return { 
+          workouts: action.payload 
+        }
+      case 'CREATE_WORKOUT':
+        return { 
+          workouts: [action.payload, ...state.workouts] 
+        }
+      case 'DELETE_WORKOUT':
+        return { 
+          workouts: state.workouts.filter((workout)=>workout._id !== action.payload._id)
+        }
+      default:
+        return state
+    }
+  }
+
+export const useWorkoutContext = () => {
+  const context = useContext(WorkoutContext)
+
+  if(!context) {
+    throw Error('useWorkoutsContext must be used inside an WorkoutsContextProvider')
+  }
+
+  return context
+}
+
+export const WorkoutContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(workoutReducer, { 
+      workouts: null
+    })
+    
+    return (
+      <WorkoutContext.Provider value={{ ...state, dispatch }}>
+        { children }
+      </WorkoutContext.Provider>
+    )
+  }
+
